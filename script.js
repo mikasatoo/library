85aa591047827f197e25bd395e93f4c14e6dc6e3
Mika Sato <mika99@live.com.au>
2023-06-28 18:37:33 +1000

    Add styling and most functionality for pop-up form

--- script.js
+++ script.js
@@ -27,32 +27,73 @@ Book.prototype.getGenres = function() {
     };
 };
 
+// References to HTML elements
+const addBookBtn = document.getElementById("add-book-btn");
+const overlay = document.getElementById("overlay");
+const addBookPopUp = document.querySelector(".add-book-pop-up");
+const formBtn = document.getElementById("add-book-form-btn")
+const resetBtn = document.getElementById("reset-btn")
+const readCheckBox = document.getElementById("read");
+const review = document.querySelector(".review");
+const totalBooks = document.getElementById("total-books-value");
+const readBooks = document.getElementById("read-books-value");
+const ratingSlider = document.getElementById("rating-slider");
+const ratingValue = document.getElementById("rating-value");
+
+// Global variables
+let totalBooksValue = 0;
+let readBooksValue = 0;
+let rating = 3;
+
 // ***** Function to add book to library
 function addBookToLibrary() {
   // ...
 };
 
-// References to HTML elements
-const addBookBtn = document.getElementById("add-book-btn");
-const addBookPopUp = document.querySelector(".add-book-pop-up");
-const readCheckBox = document.getElementById("read");
-const review = document.querySelector(".review");
-const totalBooks = document.getElementById("total-books-value");
-const readBooks = document.getElementById("read-books-value");
-
-// Global variables
-let totalBooksValue = 0;
-let readBooksValue = 0;
-
-// Function to add event listener (click) to addBookBtn to make addBookPopUp appear
-function makeAddBookAppear() {
-    addBookBtn.addEventListener("click", () => {
-        addBookPopUp.classList.add("active");
+// ***** Function to display each book on its own card
+function displayBooks() {
+    myLibrary.forEach((book) => {
+        // ...
     });
 };
 
-// Function to add event listener (change) to readCheckBox to make the review div appear
-function makeReviewAppear() {
+// ***** Function to update summary data (totalBooksValue and readBooksValue + update textContent)
+    // need to consider books being added / removed and also being checked / unchecked as read on their cards
+
+
+// Add event listener (click) to addBookBtn to open addBookPopUp and add overlay
+function openAddBookPopUp() {
+    addBookBtn.addEventListener("click", () => {
+        addBookPopUp.classList.add("active");
+        overlay.classList.add("active");
+    });
+};
+
+// ***** Add event listener (click) to formBtn and overlay to close addBookPopUp and remove overlay
+function closeAddBookPopUp() {
+    formBtn.addEventListener("click", () => {
+        // add an if statement for the form validation or change event listener to something to do with submitting (since that will only be allowed after form is validated)
+        addBookPopUp.classList.remove("active");
+        overlay.classList.remove("active");
+
+        // also need to call function/s to create book object from inputted data
+    });
+
+    overlay.addEventListener("click", () => {
+        addBookPopUp.classList.remove("active");
+        overlay.classList.remove("active");
+    });
+};
+
+// ***** Add event listener (click) to resetBtn to reset any inputted book data
+function resetBookData() {
+    resetBtn.addEventListener("click", () => {
+        // ...
+    });
+};
+
+// Add event listener (change) to readCheckBox to make the review div appear or disappear
+function toggleReviewFields() {
     readCheckBox.addEventListener("change", () => {
         if (readCheckBox.checked == true) {
             review.classList.add("active");
@@ -62,17 +103,24 @@ function makeReviewAppear() {
     });
 };
 
-// ***** Function to display each book
-function displayBooks() {
-    myLibrary.forEach((book) => {
-        // ...
-    }) 
+// Add event listener (input) to ratingSlider
+function changeRating() {
+    ratingSlider.addEventListener("input", () => {
+        console.log(ratingSlider.value);
+        rating = ratingSlider.value;
+        ratingValue.textContent = rating;
+    });
 };
 
+// ***** Add event listener (click) to removeBtn on book cards
 
 
 // Initialization
-makeAddBookAppear();
-makeReviewAppear();
+openAddBookPopUp();
+closeAddBookPopUp();
+resetBookData();
+toggleReviewFields();
+changeRating();
 totalBooks.textContent = totalBooksValue;   // ***** might need to reconsider setting these to zero initially?
 readBooks.textContent = readBooksValue;
+ratingValue.textContent = rating;
