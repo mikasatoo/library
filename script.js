5e31822ffa3a644f3c5c6fd8bd83e44e26a0944e
Mika Sato <mika99@live.com.au>
2023-06-29 18:25:42 +1000

    Restructure functions and create new book object

--- script.js
+++ script.js
@@ -28,40 +28,91 @@ Book.prototype.getGenres = function() {
 };
 
 // References to HTML elements
-const addBookBtn = document.getElementById("add-book-btn");
 const overlay = document.getElementById("overlay");
 const addBookPopUp = document.querySelector(".add-book-pop-up");
-const formBtn = document.getElementById("add-book-form-btn")
-const resetBtn = document.getElementById("reset-btn")
-const readCheckBox = document.getElementById("read");
-const review = document.querySelector(".review");
 const totalBooks = document.getElementById("total-books-value");
 const readBooks = document.getElementById("read-books-value");
+
+const form = document.getElementById("add-book-form");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const imageLink = document.getElementById("image-link");
+const pages = document.getElementById("pages");
+const genreOptions = document.getElementById("genres").options;
+const readCheckBox = document.getElementById("read");
+const review = document.querySelector(".review");
 const ratingSlider = document.getElementById("rating-slider");
 const ratingValue = document.getElementById("rating-value");
+const reviewText = document.getElementById("review-text");
+
+const addBookBtn = document.getElementById("add-book-btn");
+const resetBtn = document.getElementById("reset-btn");
 
 // Global variables
-let totalBooksValue = 0;
-let readBooksValue = 0;
-let rating = 3;
+let totalBooksValue = myLibrary.length;
+let readBooksValue = 0;     // ***** edit this
+let rating = ratingSlider.value;
 
-// ***** Function to add book to library
-function addBookToLibrary() {
-  // ...
+// Create new book object and add to myLibrary array
+function createNewBook() {
+    let selectedGenres = [];
+    for (let i = 0; i < genreOptions.length; i++) {
+        if (genreOptions[i].selected) {
+            selectedGenres.push(genreOptions[i].textContent);
+        };
+    };
+
+    let book = new Book(
+        title.value,
+        author.value,
+        imageLink.value,
+        pages.value,
+        readCheckBox.checked,
+        ratingSlider.value,
+        reviewText.value,
+        selectedGenres
+    );
+    console.log(book);
+    
+    myLibrary.push(book);
+};
+
+// Reset any book data in the form
+function resetFormData() {
+    title.value = "";
+    author.value = "";
+    imageLink.value = "";
+    pages.value = "";
+
+    for (let i = 0; i < genreOptions.length; i++) {
+        genreOptions[i].selected = false;
+    };
+
+    readCheckBox.checked = false;
+    review.classList.remove("active");
+    ratingSlider.value = 3;
+    ratingValue.textContent = 3;
+    reviewText.value = "";
 };
 
 // ***** Function to display each book on its own card
 function displayBooks() {
     myLibrary.forEach((book) => {
         // ...
+        // use the .getGenres() function to get the bookGenres list from the genres array
     });
 };
 
-// ***** Function to update summary data (totalBooksValue and readBooksValue + update textContent)
+// ***** Function to update summary info (totalBooksValue and readBooksValue + update textContent)
     // need to consider books being added / removed and also being checked / unchecked as read on their cards
+function updateSummary() {
+    // ...
+};
 
 
-// Add event listener (click) to addBookBtn to open addBookPopUp and add overlay
+// * EVENT LISTENER FUNCTIONS *
+
+// Open "Add new book" pop-up and add overlay when user clicks "+ Add new book" button
 function openAddBookPopUp() {
     addBookBtn.addEventListener("click", () => {
         addBookPopUp.classList.add("active");
@@ -69,33 +120,28 @@ function openAddBookPopUp() {
     });
 };
 
-// ***** Add event listener (click) to formBtn and overlay to close addBookPopUp and remove overlay
+// Close "Add new book" pop-up if user clicks outside of it
 function closeAddBookPopUp() {
-    formBtn.addEventListener("click", () => {
-        // add an if statement for the form validation or change event listener to something to do with submitting (since that will only be allowed after form is validated)
-        addBookPopUp.classList.remove("active");
-        overlay.classList.remove("active");
-
-        // also need to call function/s to create book object from inputted data
-    });
-
     overlay.addEventListener("click", () => {
         addBookPopUp.classList.remove("active");
         overlay.classList.remove("active");
     });
 };
 
-// ***** Add event listener (click) to resetBtn to reset any inputted book data
-function resetBookData() {
-    resetBtn.addEventListener("click", () => {
-        // ...
+// Close addBookPopUp, remove overlay, and add new book (and then reset data) when user submits the form
+function submitNewBook() {
+    form.addEventListener("submit", () => {
+        addBookPopUp.classList.remove("active");
+        overlay.classList.remove("active");
+        createNewBook();
+        resetFormData();
     });
 };
 
-// Add event listener (change) to readCheckBox to make the review div appear or disappear
+// Make review div appear or disappear when user changes the "Have you read this book?" checkbox
 function toggleReviewFields() {
     readCheckBox.addEventListener("change", () => {
-        if (readCheckBox.checked == true) {
+        if (readCheckBox.checked) {
             review.classList.add("active");
         } else {
             review.classList.remove("active");
@@ -103,24 +149,32 @@ function toggleReviewFields() {
     });
 };
 
-// Add event listener (input) to ratingSlider
+// Update rating value display when the slider is moved
 function changeRating() {
     ratingSlider.addEventListener("input", () => {
-        console.log(ratingSlider.value);
         rating = ratingSlider.value;
         ratingValue.textContent = rating;
     });
 };
 
-// ***** Add event listener (click) to removeBtn on book cards
+// Reset any book data in the form when user clicks "Reset info" button
+resetBtn.addEventListener("click", resetFormData);
+
+// Remove book from library if user clicks "Remove" button on the book card
+function removeBook() {
+    // ***** delete card and also remove from myLibrary array (and update summary info)
+
+};
 
 
-// Initialization
+// * INITIALIZATION *
 openAddBookPopUp();
 closeAddBookPopUp();
-resetBookData();
+submitNewBook();
 toggleReviewFields();
 changeRating();
-totalBooks.textContent = totalBooksValue;   // ***** might need to reconsider setting these to zero initially?
+removeBook();
+
+totalBooks.textContent = totalBooksValue;
 readBooks.textContent = readBooksValue;
 ratingValue.textContent = rating;
